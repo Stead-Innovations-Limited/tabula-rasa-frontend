bd72e07772410341dcd7a080c03db7af36e886e6
Ojo Tolulope <toluene1123@gmail.com>
2025-06-20 21:19:27 +0100

    Fixed SignUpForm UI

--- src/components/(auth)/signup/SignupContainer.tsx
+++ src/components/(auth)/signup/SignupContainer.tsx
@@ -3,7 +3,7 @@ import SignupForm from "./SignupForm";
 export default function SignupContainer() {
   return (
     <div className='w-full h-screen'>
-      <div className='w-full h-full md:h-fit flex items-center justify-center md:py-20'>
+      <div className='w-full flex items-center justify-center md:py-20'>
         <SignupForm />
       </div>
     </div>
