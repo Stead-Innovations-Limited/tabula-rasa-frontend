f4aa3d87e8e05e9cadaa60c83280f0e36a15cf1b
Ojo Tolulope <toluene1123@gmail.com>
2025-07-09 22:57:22 +0100

    Renamed file and resolved imports

--- src/components/(auth)/mail/VerifyEmail.tsx
+++ src/components/(auth)/mail/VerifyEmail.tsx
@@ -7,7 +7,7 @@ import {
   IoMail,
   IoIosCheckmarkCircle
 } from "@/components/icons";
-import ReSendButton from "@/components/(auth)/mail/ReSendButton";
+import ReSendButton from "@/components/(auth)/mail/reSendButton";
 import verifyEmail from "@/server-actions/verifyEmail";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
