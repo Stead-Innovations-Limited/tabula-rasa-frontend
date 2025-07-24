b8b4950e392c6ea5ccb547b2dc3aa3e6cf5aeb22
Ojo Tolulope <toluene1123@gmail.com>
2025-07-24 10:53:42 +0100

    Logging origin error

--- src/app/auth/google/callback/route.ts
+++ src/app/auth/google/callback/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams.toString();
   const redirectBase = request.nextUrl.origin;
+  console.log("Redirect Base", redirectBase);
 
   const targetUrl = searchParams
     ? `${redirectBase}/login?${searchParams}`
