24aaa3ff987d3f550f07bc7f074194e44fff4854
Ojo Tolulope <toluene1123@gmail.com>
2025-08-01 06:55:07 +0100

    Unplugged an invalid component

--- src/components/bookings/BookingsList.tsx
+++ src/components/bookings/BookingsList.tsx
@@ -1,13 +1,13 @@
-import BookingsCard from "./BookingsCard";
+// import BookingsCard from "./BookingsCard";
 
-function BookingsList() {
-  return (
-    <div className='space-y-5 md:space-y-8 mt-5 md:mt-8'>
-      {Array.from({ length: 5 }, (_, index) => (
-        <BookingsCard key={index} />
-      ))}
-    </div>
-  );
-}
+// function BookingsList() {
+//   return (
+//     <div className='space-y-5 md:space-y-8 mt-5 md:mt-8'>
+//       {Array.from({ length: 5 }, (_, index) => (
+//         <BookingsCard key={index} />
+//       ))}
+//     </div>
+//   );
+// }
 
-export default BookingsList;
+// export default BookingsList;
