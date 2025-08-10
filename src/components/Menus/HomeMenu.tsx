7a5e06d3a814e87ea020ffe7102496f34eff4f69
Ojo Tolulope <toluene1123@gmail.com>
2025-08-10 09:45:07 +0100

    Removed fixes

--- src/components/Menus/HomeMenu.tsx
+++ src/components/Menus/HomeMenu.tsx
@@ -18,9 +18,9 @@ import {
 
 import { cn } from "@/lib/utils";
 
-function HomeMenu({ userData }: { userData: boolean }) {
+function HomeMenu({userData}: {userData: boolean}) {
   const [open, setOpen] = useState<boolean>(false);
-
+  
   return (
     <>
       <Sheet open={open} onOpenChange={setOpen}>
@@ -34,88 +34,47 @@ function HomeMenu({ userData }: { userData: boolean }) {
             <RxHamburgerMenu className='text-2xl !size-6' />
           </Button>
         </SheetTrigger>
-        <SheetContent className='flex flex-col justify-between'>
+        <SheetContent className="flex flex-col justify-between">
           <SheetHeader className='sr-only'>
             <SheetTitle>Mobile Sidebar</SheetTitle>
             <SheetDescription>
               Mobile Sidebar for on page links
             </SheetDescription>
           </SheetHeader>
-          <nav className='mt-20 flex flex-col gap-y-2'>
-            <p className='px-6 py-3'>
-              <Link
-                href={"/#about"}
-                onClick={(e) => {
-                  e.preventDefault();
-                  const targetId = "about";
-                  const el = document.getElementById(targetId);
-
-                  if (el) {
-                    el.scrollIntoView({ behavior: "smooth" });
-                  } else {
-                    // This is a fallback if we are on a different page
-                    window.location.href = `/#${targetId}`;
-                  }
-
-                  // Close after a short delay so scroll can happen first
-                  setTimeout(() => setOpen(false), 300);
-                }}
-                className={cn(
-                  "text-[#333] font-worksans font-medium text-base"
-                )}
-              >
-                About
-              </Link>
-            </p>
-            <p className='px-6 py-3'>
-              <Link
-                href={"/#offerings"}
-                onClick={(e) => {
-                  e.preventDefault();
-                  const targetId = "offerings";
-                  const el = document.getElementById(targetId);
-
-                  if (el) {
-                    el.scrollIntoView({ behavior: "smooth" });
-                  } else {
-                    // I use this fallback if we are on a different page
-                    window.location.href = `/#${targetId}`;
-                  }
-
-                  // Close after a short delay so scroll can happen first
-                  setTimeout(() => setOpen(false), 300);
-                }}
-                className={cn(
-                  "text-[#333] font-worksans font-medium text-base"
-                )}
-              >
-                Offerings
-              </Link>
-            </p>
-            <p
-              className='px-6 py-3'
-              onClick={() => setTimeout(() => setOpen(false), 400)}
+          <nav className='mt-20 flex flex-col gap-y-2 text-[#333] font-worksans font-medium text-base'>
+            <Link
+              href={"/#about"}
+              className={cn(
+                "px-6 py-3"
+              )}
             >
-              <Link
-                href={"/contact"}
-                className={cn(
-                  "text-[#333] font-worksans font-medium text-base"
-                )}
-              >
-                Contact
-              </Link>
-            </p>
+              About
+            </Link>
+            <Link
+              href={"/#offerings"}
+              className={cn(
+                "px-6 py-3"
+              )}
+            >
+              Offerings
+            </Link>
+            <Link
+              href={"/contact"}
+              className={cn(
+                "px-6 py-3"
+              )}
+            >
+              Contact
+            </Link>
           </nav>
           <div className=''>
             <div className='p-6'>
               {userData ? (
-                <Button
-                  className='w-full bg-red-500/20 hover:bg-red-500/30'
-                  onClick={async () => {
-                    setOpen(false);
-                    // Handle logout logic here
-                    await signOut({ callbackUrl: "/" });
-                  }}
+                <Button className='w-full bg-red-500/20 hover:bg-red-500/30'
+                onClick={async () => {
+                  // Handle logout logic here
+                  await signOut({ callbackUrl: "/" }); 
+                }}
                 >
                   <FiLogOut className='text-red-500' />
                   <span className='font-dm_sans font-normal text-base text-red-500'>
@@ -123,13 +82,7 @@ function HomeMenu({ userData }: { userData: boolean }) {
                   </span>
                 </Button>
               ) : (
-                <Link
-                  href='/login'
-                  className={cn(
-                    "w-full block !bg-olive !hover:bg-olive text-xl text-white py-4 px-10",
-                    buttonVariants()
-                  )}
-                >
+                <Link href="/login" className={cn("w-full block !bg-olive !hover:bg-olive text-xl text-white py-4 px-10", buttonVariants())}>
                   Log In
                 </Link>
               )}
